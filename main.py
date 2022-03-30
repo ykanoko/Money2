7bf1fc6285f54ce8df93dddb9d9f7465e99d8b12
kano <kano7101d@gmail.com>
2022-03-30 23:00:17 +0900

    ヘルプ入力時の試作3

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.message.text='ヘルプ':
+    if event.message.text=='ヘルプ':
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text='ヘルプだよん♪'))
