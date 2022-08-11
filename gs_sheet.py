ddb1bbd867cf209351e36b5c90f18390624c26e3
kano <kano7101d@gmail.com>
2022-08-11 18:29:41 +0900

    名前でシートを取得に変更

--- gs_sheet.py
+++ gs_sheet.py
@@ -25,7 +25,7 @@ gc = gspread.authorize(credentials)
 
 SPREADSHEET_KEY = '1AjXVHcDBE32vbCVxwTCcqzHj0olxE6UlapdigoBELGs'
 wb = gc.open_by_key(SPREADSHEET_KEY)
-ws = wb.get_worksheet('実験')
+ws = wb.worksheet('実験')
 
 date = strftime("%Y/%m/%d", time.localtime())
 month_date = strftime("%Y/%m", time.localtime())
