2e1e4c121ca62cd13d47ab9cb65f0537c173e14d
kano <kano7101d@gmail.com>
2023-01-06 19:19:54 +0900

    精算old削除

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from flask import Flask, request, abort
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent,
+    TextMessage,
+    TextSendMessage,
 )
 import os
 
@@ -20,6 +18,7 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -39,13 +38,16 @@ def callback():
     return 'OK'
 
 
+@app.route("/hello", methods=['GET'])
+def hello():
+    return "OK"
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.message.text=='ヘルプ' or event.message.text=='へるぷ':
-        help = "＜入力方法一覧＞\n" + "・「合計支出（払った人）（金額）」\n"+"2人で使ったお金を記録します。\n" + "・「支出（名前）（金額）」\n"+"個人で使ったお金を記録します。\n" + "・「収入（名前）（金額）」\n"+"個人の収入を記録します。\n"+ "・「キャンセル」\n最後の項目を削除します。" 
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=help))
+    if event.message.text == 'ヘルプ' or event.message.text == 'へるぷ':
+        help = "＜入力方法一覧＞\n" + "・「合計支出（払った人）（金額）」\n" + "2人で使ったお金を記録します。\n" + "・「支出（名前）（金額）」\n" + "個人で使ったお金を記録します。\n" + "・「収入（名前）（金額）」\n" + "個人の収入を記録します。\n" + "・「キャンセル」\n最後の項目を削除します。"
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=help))
 
     #収支#
     if PERSON1_NAME in event.message.text or PERSON2_NAME in event.message.text:
@@ -55,37 +57,29 @@ def handle_message(event):
             t = '支出'
         elif '収入' in event.message.text:
             t = '収入'
-        
+
         if PERSON1_NAME in event.message.text:
             p = PERSON1_NAME
-            m = int(event.message.text[len(t)+len(p):])
+            m = int(event.message.text[len(t) + len(p):])
             n = 0
         else:
             p = PERSON2_NAME
             m = 0
-            n = int(event.message.text[len(t)+len(p):])
-            
+            n = int(event.message.text[len(t) + len(p):])
+
         try:
-            money = money_gs_sheet(t,m,n,p)
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=money))
+            money = money_gs_sheet(t, m, n, p)
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=money))
         except Exception as error:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=str(error)))
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str(error)))
 
     #キャンセル#
     if event.message.text == 'キャンセル':
         try:
             cancel = cancel_gs_sheet()
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=cancel))
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=cancel))
         except Exception as error:
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=str(error)))
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str(error)))
 
     # #精算#
     # #if event.message.text == '精算':
@@ -103,6 +97,6 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-#    app.run()
+    #    app.run()
     port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)     
+    app.run(host="0.0.0.0", port=port)
